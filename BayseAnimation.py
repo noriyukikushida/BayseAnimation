96a426d23dd41d081113945b43e840a2e0ab3268
Noriyuki Kushida <kushida@dlw053.idc.ctbto.org>
2019-06-07 09:36:49 +0000

    showing the hypocentre which has the best score so far

--- BayseAnimation.py
+++ BayseAnimation.py
@@ -78,6 +78,7 @@ for i in range(100):
 	score[i]=0.
 	hits[i]=0
 max_score=0.
+max_loc = 0.0
 high_score=0
 x00=500
 for j in range(1000):
@@ -92,6 +93,15 @@ for j in range(1000):
 	pause=5
 	if hits[x0/10] > 0 : 
 		continue
+
+        p_max_score = -1.0
+        p_max_loc   = -1.0
+        for ipos in xrange(100):
+           if hits[ipos] > 0:
+             if score[ipos] > p_max_score:
+                p_max_score = score[ipos]
+                p_max_loc   = ipos*10
+
 	hits[x0/10] += 1
 	score[x0/10]=p1*p2
 	if(score[x0/10] > 1.):
@@ -106,21 +116,23 @@ for j in range(1000):
 		
 	if(score[x0/10] > max_score):
 		max_score=score[x0/10]
+                max_loc=x0
 
         iStation1 = 0
         iStation2 = 0
 	for i in range(300):
 		rad=circle_radius+i*dr + 0.01
 		chart_1.create_line(0,100,1000,100,width=3,fill="black")
+	        if(p_max_score > 0.0):
+	        	chart_1.create_oval(p_max_loc-15,85,p_max_loc+15,115,fill="yellow")
+                        printText = "Candidate\nScore:{0:.2e}".format(p_max_score)
+		        chart_1.create_text(p_max_loc,125,text=printText,fill="black",font=("Times Roman","16"))
 		for k in range(99):
 			chart_1.create_line(10*k,int(200.*(1.-lap1[k])),10*(k+1),int(200.*(1.-lap1[k+1])),width=2,fill="red")
 			chart_1.create_line(10*k,int(200.*(1.-lap2[k])),10*(k+1),int(200.*(1.-lap2[k+1])),width=2,fill="blue")
 		chart_1.create_oval(x0-rad,y0-rad,x0+rad,y0+rad,outline="violet",width=3)
 		chart_1.create_rectangle(290,90,310,110,fill="red")
 		chart_1.create_text(300,80,text="Station 1",fill="black",font=("Times Roman","16"))
-		if(max_score > 0.6):
-			chart_1.create_oval(490,90,510,110,fill="yellow")
-			chart_1.create_text(500,80,text="Source",fill="black",font=("Times Roman","16"))
 		chart_1.create_rectangle(690,90,710,110,fill="blue")
 		chart_1.create_text(700,80,text="Station 2",fill="black",font=("Times Roman","16"))
 		chart_1.create_oval(x0-10,y0-10,x0+10,y0+10,fill="violet")				
@@ -138,20 +150,29 @@ for j in range(1000):
                    pos = 300 - (abs(500-300)           - abs(x0-300))
 		   chart_1.create_line(pos,100,pos,200,width=2,fill="red")
 		   chart_1.create_line(300,150,pos,150,width=2,fill="red",arrow=LAST)
-                   printText = str( (pos-300)/10 )+"sec."
-		   chart_1.create_text(300,170,text=printText,fill="black",font=("Times Roman","16"))
+                   printText = "{0:+d}{1}\nScore1={2:.2f}".format((pos-300)/10 ,"sec.",p1)
+		   chart_1.create_text(300,30,text=printText,fill="black",font=("Times Roman","16"))
+		   chart_2.create_line(x0,0,x0,200,width=2,fill="yellow")
                 if abs(rad - abs(700-x0)) < 10.0:
                    #pos = 700 - (x0 - 500)
                    pos = 700 - (abs(500-700)           - abs(x0-700))
 		   chart_1.create_line(pos,100,pos,200,width=2,fill="blue")
 		   chart_1.create_line(700,150,pos,150,width=2,fill="blue",arrow=LAST)
-                   printText = str( (pos-700)/10)+"sec."
-		   chart_1.create_text(700,170,text=printText,fill="black",font=("Times Roman","16"))
+                   printText = "{0:+d}{1}\nScore2={2:.2f}".format((pos-700)/10 ,"sec.",p2)
+		   chart_1.create_text(700,30,text=printText,fill="black",font=("Times Roman","16"))
+		   chart_2.create_line(x0,0,x0,200,width=2,fill="yellow")
 		chart_1.update()
                 if abs(rad - abs(300-x0)) < 10.0 or abs(rad-abs(700-x0)) < 10.0:
 		   chart_1.after(pause*500)
                 else:
 		   chart_1.after(pause)
+
+                if rad >= abs(300-x0) and rad >= abs(700-x0):
+                   printText = "Total Score:{0:.2e}".format(p1*p2)
+		   chart_2.create_text(500,100,text=printText,fill="yellow",font=("Times Roman","16"))
+		   chart_2.create_line(x0,0,x0,200,width=2,fill="yellow")
+		   chart_2.update()
+		   chart_1.after(pause*500)
 		chart_1.delete(ALL)
 		chart_2.delete(ALL)
 
